b7aa90fa933d16acf398765b3136925e6fd499c7
rockettcs <89966003+rockettcs@users.noreply.github.com>
2023-01-10 12:44:05 +0530

    Update GreeterControllerTest.java

--- springboot/src/test/java/com/redhat/developer/demos/GreeterControllerTest.java
+++ springboot/src/test/java/com/redhat/developer/demos/GreeterControllerTest.java
@@ -24,6 +24,6 @@ public class GreeterControllerTest {
   @Test
   public void greetingShouldReturnDefaultMessage() throws Exception {
     assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
-        .contains("Meeow!! from  😺🚀");
+        .contains("Meeow!!  😺🚀");
   }
 }
